1560cad1436a54330f2d8b34f08258e9548fea21
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-11 11:01:12 +0430

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -190,7 +190,7 @@ resource "aws_apprunner_service" "backend_service" {
          # build_command = "pip install -r backend/requirements.txt"
           build_command = "python3 -m pip install -r ./backend/requirements.txt"
         #   start_command = "node ./backend/server.js" 
-          start_command = "uvicorn backend.main:app --host 0.0.0.0 --port 8080" 
+          start_command = "python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8080" 
           port           = 8080
           runtime_environment_variables = {
             NODE_ENV        = "production"
